b8b35d0cc06875456e723af6bdc0d05e7a961b3f
Arif Ahmed <kh355680@hotmail.com>
2017-02-04 16:35:10 +0600

    form modal to add new todo done

--- src/script/todo.js
+++ src/script/todo.js
@@ -1,31 +1,74 @@
+var vm = this;
+
 var todo = {
     title: '',
     description: ''
 }
 
-var todos = [{
-        title: 'Todo Task',
-        description: 'blah blah blah blah blah blah blah blah blah.............',
-        status: true
-    },
-    {
-        title: 'Todo Task',
-        description: 'blah blah blah blah blah blah blah blah blah.............',
-        status: true
-    },
-    {
-        title: 'Todo Task',
-        description: 'blah blah blah blah blah blah blah blah blah.............',
-        status: true
-    }
-];
+vm.todos = [];
 
 function windowLoadEventHandler() {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var data = JSON.parse(this.responseText);
+            vm.todos = data.result;
+            console.log('Result', vm.todos);
+
+            var todoListDiv = document.getElementById('todo-list');
+            for (var i = 0; i < vm.todos.length; i++) {
+
+                var todoItem = document.createElement('div');
+                var titleSpan = document.createElement('span');
+                titleSpan.innerText = vm.todos[i]["title"];
+                todoItem.appendChild(titleSpan);
+
+                var x = document.createElement("INPUT");
+                x.setAttribute("type", "checkbox");
+                todoItem.appendChild(x);
+
+                var todoContent = document.createElement('div');
+                var description = document.createElement('span');
+                description.innerText = vm.todos[i]["description"];
+                todoContent.appendChild(description);
+
+                var operation = document.createElement('span');
+                var editBtn = document.createElement("INPUT");
+                editBtn.setAttribute("type", "button");
+                editBtn.value = 'Edit';
+                operation.appendChild(editBtn);
+
+                var deleteBtn = document.createElement("INPUT");
+                deleteBtn.setAttribute("type", "button");
+                deleteBtn.value = 'Delete';
+                operation.appendChild(deleteBtn);
+
+                todoContent.appendChild(operation);
+                todoItem.appendChild(todoContent);
+
+                todoListDiv.appendChild(todoItem);
+
+                var hr = document.createElement('hr');
+                todoListDiv.appendChild(hr);
+
+            }
+
+        }
+    };
+    xhttp.open("GET", "src/assets/data.json", true);
+    xhttp.send();
+}
+
+function test() {
+
+
     var todoListDiv = document.getElementById('todo-list');
 
     var todoItem = document.createElement('div');
     var titleSpan = document.createElement('span');
-    titleSpan.innerText = 'Title';
+    titleSpan.innerText = 'TTT';
     todoItem.appendChild(titleSpan);
 
     var x = document.createElement("INPUT");
@@ -34,7 +77,7 @@ function windowLoadEventHandler() {
 
     var todoContent = document.createElement('div');
     var description = document.createElement('span');
-    description.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    description.innerText = 'TTT';
     todoContent.appendChild(description);
 
     var operation = document.createElement('span');
@@ -52,4 +95,7 @@ function windowLoadEventHandler() {
     todoItem.appendChild(todoContent);
 
     todoListDiv.appendChild(todoItem);
+
+    var hr = document.createElement('hr');
+    todoListDiv.appendChild(hr);
 }
